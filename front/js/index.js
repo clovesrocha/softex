534423e574f9ab1e246587fa6d375dc40524914c
Marlon Paulo <marlonpaulo1@gmail.com>
2022-11-28 15:42:15 -0300

    Create index.js

---
+++ front/js/index.js
@@ -0,0 +1,27 @@
+import data from '../database/data.js'
+
+const cards = document.querySelector('.cards')
+
+function getCard() {
+    let displayCard = data.map(function(item) {
+        return `
+        <div class="card">
+            <img src="https://github.com/${item.github}.png" alt=${item.nome}>
+            <div class="card-text">
+                <h2>${item.nome}</h2>
+                <p>${item.email}</p>
+            </div>
+            <div class="links">
+                <a href="https://github.com/${item.github}"  target="_blank"><i class="fa-brands fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/${item.linkedin}"  target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="https://www.instagram.com/${item.instagram}"  target="_blank"><i class="fa-brands fa-instagram"></i></a>
+            </div>
+            <a href=${item.page} target="_blank" class="btn-link">Profile</a>
+        </div>
+        `
+    })
+    displayCard = displayCard.join('')
+    cards.innerHTML = displayCard
+}
+
+getCard()
