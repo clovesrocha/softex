3d69b166a774c7465589c54cc2f631b4b43fac18
Marlon Paulo <marlonpaulo1@gmail.com>
2022-11-28 15:44:24 -0300

    Create data.js

---
+++ front/database/data.js
@@ -0,0 +1,74 @@
+export default  [
+    {
+        nome: 'Arthur Martins',
+        email: '',
+        github: 'martins8',
+        linkedin: '',
+        instagram: '',
+        page: ''
+    },
+    {
+        nome: 'Elis Mendes',
+        email: '',
+        github: 'elismendes',
+        linkedin: '',
+        instagram: '',
+        page: ''
+    },
+    {
+        nome: 'Flora Fernandes Martins',
+        email: '',
+        github: 'florafernandes',
+        linkedin: '',
+        instagram: '',
+        page: ''
+    },
+    {
+        nome: 'Heitor França',
+        email: '',
+        github: 'Heitorfrc',
+        linkedin: '',
+        instagram: '',
+        page: ''
+    },
+    {
+        nome: 'João Ferreira',
+        email: '',
+        github: 'JoaoFerreira1000',
+        linkedin: '',
+        instagram: '',
+        page: ''
+    },
+    {
+        nome: 'Juliana S.',
+        email: '',
+        github: 'julianabrunaah',
+        linkedin: '',
+        instagram: '',
+        page: ''
+    },
+    {
+        nome: 'Luciana Guimarães Mariz',
+        email: '',
+        github: 'LucianaGuimaraesMariz',
+        linkedin: '',
+        instagram: '',
+        page: ''
+    },
+    {
+        nome: 'Marlon Paulo',
+        email: 'marlonpaulo1@gmail.com',
+        github: 'marlonpaulo1',
+        linkedin: 'marlon-paulo-silva-amaral',
+        instagram: 'marlon_paulo1',
+        page: './pages/marlon.html'
+    },
+    {
+        nome: 'Matheus Dutra',
+        email: '',
+        github: 'matheusdutrag',
+        linkedin: '',
+        instagram: '',
+        page: ''
+    },
+]
