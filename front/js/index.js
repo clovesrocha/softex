d5e56359c5491a5e2346024691fc97719ab2f877
Marlon Paulo <marlonpaulo1@gmail.com>
2022-12-12 15:15:20 -0300

    Update index.js

--- front/js/index.js
+++ front/js/index.js
@@ -1,5 +1,21 @@
 import data from '../database/data.js'
 
+const menuBtn = document.querySelector('.toggle')
+const menuTop = document.querySelector('.links ul')
+let menuOpen = false
+
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open')
+        menuTop.classList.add('active')
+        menuOpen = true
+    } else {
+        menuBtn.classList.remove('open')
+        menuTop.classList.remove('active')
+        menuOpen = false
+    }
+})
+
 const cards = document.querySelector('.cards')
 
 function getCard() {
